027ce4086245c8e89184baf79456f167de5cacb6
Ben Bright <nooc@users.noreply.github.com>
2024-05-31 03:21:29 +0200

    fix bug for get user/data. was old code

--- parking-bot/app/endpoints/user.py
+++ parking-bot/app/endpoints/user.py
@@ -1,5 +1,6 @@
+import logging
 from datetime import UTC, datetime, timedelta
-from typing import Any, AnyStr
+from typing import Any
 
 import jwt
 import jwt.utils
@@ -8,7 +9,13 @@ from fastapi.responses import PlainTextResponse
 
 import app.util.http_error as err
 from app.config import conf
-from app.dependencies import get_db, get_jwt, get_user, get_user_manager
+from app.dependencies import (
+    get_db,
+    get_jwt,
+    get_user,
+    get_user_manager,
+    get_userdata_manager,
+)
 from app.endpoints.media_types import MEDIA_TYPE_JSON
 from app.models.carpark import *
 from app.models.response.userdata import UserData
@@ -16,8 +23,11 @@ from app.models.user import User, UserUpdate
 from app.models.vehicle import Vehicle, VehicleDb
 from app.services.datastore import Database
 from app.services.user_manager import UserManager
+from app.services.userdata_manager import UserdataManager
 
 router = APIRouter()
+
+__IDENTIFIER = "identifier"
 
 
 @router.get("", status_code=status.HTTP_200_OK)
@@ -39,12 +49,12 @@ def init_user(
     um: UserManager = Depends(get_user_manager),
     jwtd: dict = Depends(get_jwt),
 ) -> str:
-    if not "identifier" in jwtd:
+    if not __IDENTIFIER in jwtd:
         err.bad_request("No identifier")
     id_ok = False
     # android secure device id is 64bit hex string
     try:
-        int(jwtd["identifier"], 16)
+        int(jwtd[__IDENTIFIER], 16)
         id_ok = True
     except:
         pass
@@ -56,11 +66,12 @@ def init_user(
     if not id_ok:
         err.bad_request()
     try:
-        user = um.get_user(jwtd["identifier"])
+        user = um.get_user(jwtd[__IDENTIFIER])
     except:
         user = None
     if not user:
-        user = um.create_user(Id=jwtd["identifier"])
+        logging.info('Creating user "%s"', jwtd[__IDENTIFIER])
+        user = um.create_user(Id=jwtd[__IDENTIFIER])
     if not user:
         err.internal("Could not create user.")
     dt = datetime.now(UTC)
@@ -96,24 +107,19 @@ def delete_user(
 
 @router.get("/data", status_code=status.HTTP_200_OK)
 def get_settings(
-    db: Database = Depends(get_db), current_user: User = Depends(get_user)
+    udata: UserdataManager = Depends(get_userdata_manager),
+    current_user: User = Depends(get_user),
 ) -> UserData:
     try:
-        vehicles = db.get_objects_by_query(
-            VehicleDb, filters=[("UserId", "=", current_user.Id)]
-        )
-        toll = db.get_objects_by_query(
-            SelectedTollParkDb, filters=[("UserId", "=", current_user.Id)]
-        )
-        kiosk = db.get_objects_by_query(
-            SelectedKioskParkDb, filters=[("UserId", "=", current_user.Id)]
-        )
+        vehicles = udata.list_vehicles(current_user)
+        tolls = udata.list_toll_carparks(current_user)
+        kiosks = udata.list_kiosk_carparks(current_user)
         return UserData(
             User=current_user,
             Vehicles=[Vehicle(**v.model_dump()) for v in vehicles],
             CarParks=CarParks(
-                Toll=[SelectedTollPark(**t.model_dump()) for t in toll],
-                Kiosk=[SelectedKioskPark(**k.model_dump()) for k in kiosk],
+                Toll=[SelectedTollPark(**t.model_dump()) for t in tolls],
+                Kiosk=[SelectedKioskPark(**k.model_dump()) for k in kiosks],
             ),
         )
     except:
